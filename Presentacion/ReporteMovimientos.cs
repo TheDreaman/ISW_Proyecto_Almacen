8860066c5546248b09e4f53163e8fa3abbb81db4
Esthela Axi <Esthela_axi@outlook.com>
2021-01-06 20:33:43 -0600

    Precentación final de la ventana Reporte de Movimientos.

--- Presentacion/ReporteMovimientos.cs
+++ Presentacion/ReporteMovimientos.cs
@@ -16,5 +16,41 @@ namespace Presentacion
         {
             InitializeComponent();
         }
+        //Declaracion de parametros
+        public DateTime Fecha1 { get; set; }
+        public DateTime Fecha2 { get; set; }
+
+        private void ReporteMovimientos_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'DataSetMovimientoFecha.Rango_fecha' Puede moverla o quitarla según sea necesario.
+            this.Rango_fechaTableAdapter.Fill(this.DataSetMovimientoFecha.Rango_fecha, Fecha1, Fecha2);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            //se declaran los paraametros
+
+            DateTime Fecha1 = dtpDesde.Value;
+            DateTime Fecha2 = dtpHasta.Value;
+            this.Rango_fechaTableAdapter.Fill(this.DataSetMovimientoFecha.Rango_fecha, Fecha1, Fecha2);
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
